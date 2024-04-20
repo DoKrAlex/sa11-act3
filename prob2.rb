e01eab342a0713fb76b851cf71e2bdb4ac93eeca
Alex Do <alexkdo03@gmail.com>
2024-04-19 22:01:52 -0500

    Commit with corrected answer to problem 2, invoice parser

--- prob2.rb
+++ prob2.rb
@@ -1,8 +1,14 @@
-def extract_urls(text)
-  pattern = %r{(?i)\b((?:https?|ftp)://\S+(?:\.\S+)*\b)}
-  urls = text.scan(pattern)
-  urls.each { |url| puts url }
+def parse_invoices(invoice_data)
+  invoice_data.each_line do |invoice|
+    match_data = invoice.match(/(\d{4}-\d{2}-\d{2}) - ([\w\d]+) - ([\w\s]+) - (\$\d+(\.\d+)?)/)
+    puts "Date: #{match_data[1]}, Invoice Number: #{match_data[2]}, Client: #{match_data[3]}, Amount: #{match_data[4]}"
+  end
 end
 
-sample_text = "Visit our site at http://www.example.org for more information. Check out our search page at https://example.com/search?q=ruby+regex. Don’t forget to ftp our resources at ftp://example.com/resources."
-extract_urls(sample_text)
+invoice_entries = <<-INVOICES
+2023-03-01 - INV001 - Acme Corp - $1000
+2023-03-02 - INV002 - Beta LLC - $2050
+2023-03-03 - INV003 - Gamma Inc - $3500
+INVOICES
+
+parse_invoices(invoice_entries)
